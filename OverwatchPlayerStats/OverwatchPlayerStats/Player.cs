a67d464555af1df436954f39bc9a197cc55682cb
Kareem Yacoub <karyacoub@gmail.com>
2019-01-03 18:34:07 -0600

    removed visibility struct in player class to match the updated player search api

--- OverwatchPlayerStats/OverwatchPlayerStats/Player.cs
+++ OverwatchPlayerStats/OverwatchPlayerStats/Player.cs
@@ -2,38 +2,21 @@
 
 namespace OverwatchPlayerStats
 {
-    struct Visibility
-    {
-        public string name { get; set; }
-        public bool isPublic { get; set; }
-        public bool isPrivate { get; set; }
-        public bool isFriendsOnly { get; set; }
-
-        public Visibility(Visibility visibility)
-        {
-            name = visibility.name;
-            isPublic = visibility.isPublic;
-            isPrivate = visibility.isPrivate;
-            isFriendsOnly = visibility.isFriendsOnly;
-        }
-    }
-
     class Player
     {
+        [JsonProperty("platform")]
         public string platform { get; set; }
-        public string urlName { get; set; }
-        public int level { get; set; }
-        public string portrait { get; set; }
-        public Visibility visibility;
 
-        [JsonConstructor]
-        public Player(string platform, string urlName, int level, string portrait, Visibility visibility)
-        {
-            this.platform = platform;
-            this.urlName = urlName;
-            this.level = level;
-            this.portrait = portrait;
-            this.visibility = new Visibility(visibility);
-        }
+        [JsonProperty("urlName")]
+        public string urlName { get; set; }
+
+        [JsonProperty("level")]
+        public int level { get; set; }
+
+        [JsonProperty("portrait")]
+        public string portrait { get; set; }
+
+        [JsonProperty("isPublic")]
+        public bool isPublic { get; set; }
     }
 }
